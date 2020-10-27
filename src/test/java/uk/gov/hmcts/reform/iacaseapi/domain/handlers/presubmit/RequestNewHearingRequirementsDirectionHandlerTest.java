ae417a65b4e6512db3ef86ede1ee5cbebc368cfb
Brad Sorour <brad.sorour@gmail.com>
2020-10-27 09:29:57 +0000

    RIA-3555 merge

---
+++ src/test/java/uk/gov/hmcts/reform/iacaseapi/domain/handlers/presubmit/RequestNewHearingRequirementsDirectionHandlerTest.java
@@ -0,0 +1,211 @@
+package uk.gov.hmcts.reform.iacaseapi.domain.handlers.presubmit;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static uk.gov.hmcts.reform.iacaseapi.domain.entities.AsylumCaseFieldDefinition.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.iacaseapi.domain.DateProvider;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.*;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.CaseDetails;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iacaseapi.domain.entities.ccd.field.IdValue;
+import uk.gov.hmcts.reform.iacaseapi.domain.service.DirectionAppender;
+import uk.gov.hmcts.reform.iacaseapi.domain.service.FeatureToggler;
+import uk.gov.hmcts.reform.iacaseapi.domain.service.PreviousHearingAppender;
+
+@RunWith(MockitoJUnitRunner.class)
+@SuppressWarnings("unchecked")
+public class RequestNewHearingRequirementsDirectionHandlerTest {
+
+    @Mock private DateProvider dateProvider;
+    @Mock private DirectionAppender directionAppender;
+    @Mock private PreviousHearingAppender previousHearingAppender;
+    @Mock private Callback<AsylumCase> callback;
+    @Mock private CaseDetails<AsylumCase> caseDetails;
+    @Mock private AsylumCase asylumCase;
+    @Mock private FeatureToggler featureToggler;
+
+    private static final int HEARING_REQUIREMENTS_DUE_IN_DAYS = 5;
+    private RequestNewHearingRequirementsDirectionHandler requestNewHearingRequirementsDirectionHandler;
+
+    @Before
+    public void setUp() {
+        requestNewHearingRequirementsDirectionHandler =
+            new RequestNewHearingRequirementsDirectionHandler(
+                HEARING_REQUIREMENTS_DUE_IN_DAYS,
+                dateProvider,
+                directionAppender,
+                previousHearingAppender,
+                featureToggler
+            );
+    }
+
+    @Test
+    public void can_handle_request_new_hearing_requirements() {
+
+        when(callback.getEvent()).thenReturn(Event.REQUEST_NEW_HEARING_REQUIREMENTS);
+        when(featureToggler.getValue("reheard-feature", false)).thenReturn(true);
+
+        requestNewHearingRequirementsDirectionHandler =
+            new RequestNewHearingRequirementsDirectionHandler(
+                HEARING_REQUIREMENTS_DUE_IN_DAYS,
+                dateProvider,
+                directionAppender,
+                previousHearingAppender,
+                featureToggler
+            );
+
+        boolean canHandle = requestNewHearingRequirementsDirectionHandler.canHandle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
+
+        assertTrue(canHandle);
+    }
+
+    @Test
+    public void should_append_new_direction_to_existing_directions_for_the_case() {
+
+        final List<IdValue<Direction>> existingDirections = new ArrayList<>();
+        final List<IdValue<Direction>> allDirections = new ArrayList<>();
+
+        final String expectedExplanation = "Do the thing";
+        final Parties expectedParties = Parties.LEGAL_REPRESENTATIVE;
+        final String expectedDateDue = "2020-10-06";
+        final DirectionTag expectedDirectionTag = DirectionTag.REQUEST_NEW_HEARING_REQUIREMENTS;
+        final Event event = Event.REQUEST_NEW_HEARING_REQUIREMENTS;
+        final String appellantNameForDisplay = "Joe Bloggs";
+        final String respondentRepresentative = "Jim Smith";
+        final String ariaListingReference = "LP/12345/2020";
+        final HearingCentre listCaseHearingCentre = HearingCentre.TAYLOR_HOUSE;
+        final String listCaseHearingDate = "13/10/2020";
+        final String listCaseHearingLength = "6 hours";
+        final String appealDecision = "Dismissed";
+
+        when(featureToggler.getValue("reheard-feature", false)).thenReturn(true);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(callback.getEvent()).thenReturn(event);
+        when(caseDetails.getCaseData()).thenReturn(asylumCase);
+        when(dateProvider.now()).thenReturn(LocalDate.parse("2020-10-01"));
+
+        when(asylumCase.read(DIRECTIONS)).thenReturn(Optional.of(existingDirections));
+        when(asylumCase.read(SEND_DIRECTION_EXPLANATION, String.class)).thenReturn(Optional.of(expectedExplanation));
+        when(asylumCase.read(SEND_DIRECTION_DATE_DUE, String.class)).thenReturn(Optional.of(expectedDateDue));
+        when(asylumCase.read(APPELLANT_NAME_FOR_DISPLAY, String.class)).thenReturn(Optional.of(appellantNameForDisplay));
+        when(asylumCase.read(RESPONDENT_REPRESENTATIVE, String.class)).thenReturn(Optional.of(respondentRepresentative));
+        when(asylumCase.read(ARIA_LISTING_REFERENCE, String.class)).thenReturn(Optional.of(ariaListingReference));
+        when(asylumCase.read(LIST_CASE_HEARING_CENTRE, HearingCentre.class)).thenReturn(Optional.of(listCaseHearingCentre));
+        when(asylumCase.read(LIST_CASE_HEARING_DATE, String.class)).thenReturn(Optional.of(listCaseHearingDate));
+        when(asylumCase.read(LIST_CASE_HEARING_LENGTH, String.class)).thenReturn(Optional.of(listCaseHearingLength));
+        when(asylumCase.read(APPEAL_DECISION, String.class)).thenReturn(Optional.of(appealDecision));
+
+        when(directionAppender.append(
+            existingDirections,
+            expectedExplanation,
+            expectedParties,
+            expectedDateDue,
+            expectedDirectionTag
+        )).thenReturn(allDirections);
+
+        PreSubmitCallbackResponse<AsylumCase> callbackResponse =
+            requestNewHearingRequirementsDirectionHandler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
+
+        assertNotNull(callbackResponse);
+        assertEquals(asylumCase, callbackResponse.getData());
+
+        verify(directionAppender, times(1)).append(
+            eq(existingDirections),
+            contains(expectedExplanation),
+            eq(expectedParties),
+            eq(expectedDateDue),
+            eq(expectedDirectionTag)
+        );
+
+        verify(asylumCase, times(1)).read(SEND_DIRECTION_EXPLANATION, String.class);
+        verify(asylumCase, times(1)).read(SEND_DIRECTION_DATE_DUE, String.class);
+        verify(asylumCase, times(1)).write(DIRECTIONS, allDirections);
+    }
+
+    @Test
+    public void handling_should_throw_if_cannot_actually_handle() {
+
+        assertThatThrownBy(() -> requestNewHearingRequirementsDirectionHandler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback))
+            .hasMessage("Cannot handle callback")
+            .isExactlyInstanceOf(IllegalStateException.class);
+
+        assertThatThrownBy(() -> requestNewHearingRequirementsDirectionHandler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback))
+            .hasMessage("Cannot handle callback")
+            .isExactlyInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    public void it_can_handle_callback() {
+
+        for (Event event : Event.values()) {
+
+            when(callback.getEvent()).thenReturn(event);
+            when(featureToggler.getValue("reheard-feature", false)).thenReturn(true);
+
+            for (PreSubmitCallbackStage callbackStage : PreSubmitCallbackStage.values()) {
+
+                boolean canHandle = requestNewHearingRequirementsDirectionHandler.canHandle(callbackStage, callback);
+
+                if (event == Event.REQUEST_NEW_HEARING_REQUIREMENTS
+                    && callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT) {
+
+                    assertTrue(canHandle);
+                } else {
+                    assertFalse(canHandle);
+                }
+            }
+        }
+
+        for (Event event : Event.values()) {
+
+            when(callback.getEvent()).thenReturn(event);
+            when(featureToggler.getValue("reheard-feature", false)).thenReturn(false);
+
+            for (PreSubmitCallbackStage callbackStage : PreSubmitCallbackStage.values()) {
+
+                boolean canHandle = requestNewHearingRequirementsDirectionHandler.canHandle(callbackStage, callback);
+
+                if (event == Event.REQUEST_NEW_HEARING_REQUIREMENTS
+                    && callbackStage == PreSubmitCallbackStage.ABOUT_TO_START) {
+
+                    assertFalse(canHandle);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void should_not_allow_null_arguments() {
+
+        assertThatThrownBy(() -> requestNewHearingRequirementsDirectionHandler.canHandle(null, callback))
+            .hasMessage("callbackStage must not be null")
+            .isExactlyInstanceOf(NullPointerException.class);
+
+        assertThatThrownBy(() -> requestNewHearingRequirementsDirectionHandler.canHandle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, null))
+            .hasMessage("callback must not be null")
+            .isExactlyInstanceOf(NullPointerException.class);
+
+        assertThatThrownBy(() -> requestNewHearingRequirementsDirectionHandler.handle(null, callback))
+            .hasMessage("callbackStage must not be null")
+            .isExactlyInstanceOf(NullPointerException.class);
+
+        assertThatThrownBy(() -> requestNewHearingRequirementsDirectionHandler.handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, null))
+            .hasMessage("callback must not be null")
+            .isExactlyInstanceOf(NullPointerException.class);
+    }
+}
